614aae2a4669981fa8d03c183dbf33b1a0fe3424
Sidharth <sidhuuosid@gmail.com>
2024-07-02 22:18:57 +0530

    .

--- lib/login.dart
+++ lib/login.dart
@@ -6,15 +6,114 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.horizontal(
-            left: Radius.circular(30),
-            right: Radius.circular(30),
+      backgroundColor: const Color(0xFF041444),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(25),
+            child: Image.asset('assets/logo.png'),
           ),
-          color: Color(0xFF041444),
-        ),
-        height: 400,
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 10, left: 40, right: 40),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Login',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          color: Color(0xFF041444),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(15),
+                          ),
+                        ),
+                        hintText: 'Enter Your Email Address',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    const TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(15),
+                          ),
+                        ),
+                        hintText: 'Enter Password',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: const ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(
+                          Color(0xFF041444),
+                        ),
+                      ),
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Forgot Password?',
+                        style: TextStyle(
+                            color: Color(0xFF041444),
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 80,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: RichText(
+                        text: TextSpan(
+                            text: 'Dont have an Account? ',
+                            style: TextStyle(
+                                color: Colors.grey[600],
+                                fontWeight: FontWeight.bold),
+                            children: const <TextSpan>[
+                              TextSpan(
+                                text: 'Register',
+                                style: TextStyle(
+                                    color: Color(0xFF041444),
+                                    fontWeight: FontWeight.bold),
+                              )
+                            ]),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
